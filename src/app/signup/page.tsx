24df1221a0222b6de99cdfa0117fb47673680e25
gowthambhargav <gowthambhargav26@gmail.com>
2024-02-09 16:29:59 +0530

    Refactor signup page component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -14,33 +14,7 @@ const Signup = () => {
 
   const onSignup = async () => {};
 
-  return (
-    <div>
-      create a nextjs signup componment using Tailwind CSS
-      <div>
-        <input
-          type="text"
-          placeholder="username"
-          value={user.username}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
-        />
-        <input
-          type="text"
-          placeholder="email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-        />
-        <button onClick={onSignup}>Signup</button>
-        nk
-      </div>
-    </div>
-  );
+  return <div>create a nextjs Tailwind CSS signup componment</div>;
 };
 
 export default Signup;
